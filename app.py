cc381b416c9ba7e3fdf2c88f9d828d114813ea19
Menilek <menilek25@hotmail.co.uk>
2020-03-20 11:58:19 +0000

    Implemented retrieval of fighter data from db; rendering data in template

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
-from bson import ObjectId
+from flask import Flask, render_template
+from bson import ObjectId #CURRENTLY NOT USED
 from pymongo import MongoClient
-import os
 
 app = Flask(__name__)
-app.config.from_object('config')
-client = MongoClient()
-db = client['fantasy-fighter'] #select db
-fighters = db['fighters'] #select collection
+app.config.from_object('instance.config.Config')
+client = MongoClient(app.config['DATABASE_URI'])
+print(client)
 
 title = 'Top Fighters'
 
-# fighters = [
-#     {"name" : "Menilek", "striking" : "Boxing", "grappling" : "Jiu Jitsu"},
-#     {"name" : "Venom", "striking" : "Kickboxing", "grappling" : "Jiu Jitsu"}
-# ] <-- DUMMY DATA TO PLAY WITH
-
 @app.route('/')
 def fighters():
+    db = client['fantasy-fighter'] #select db
+    fighters = db['fighters'] #select collection
     heading = 'POUND FOR POUND'
-    fighter_list = fighters.find()
+    fighter_list = fighters.find({},{"_id": 0, "name": 1, "striking": 1, "grappling": 1})
     return render_template('index.html',fighters=fighter_list, t=title, h=heading)
 
 if __name__ == '__main__':
