3a013dab96c379ae6c86cb75d60ac1ebd5e59ac8
Menilek <menilek25@hotmail.co.uk>
2020-03-21 14:04:27 +0000

    Created api endpoint /fighter-list/all returning fighter list data as json

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from bson import ObjectId #CURRENTLY NOT USED
 from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config.from_object('instance.config.Config')
 client = MongoClient(app.config['DATABASE_URI'])
-print(client)
+db = client['fantasy-fighter'] #select db
 
 title = 'Top Fighters'
 
 @app.route('/')
 def fighters():
-    db = client['fantasy-fighter'] #select db
     fighters = db['fighters'] #select collection
     heading = 'POUND FOR POUND'
     fighter_list = fighters.find({},{"_id": 0, "name": 1, "striking": 1, "grappling": 1})
     return render_template('index.html',fighters=fighter_list, t=title, h=heading)
+
+@app.route('/fighter-list/all', methods=['GET'])
+def json_fighter_list():
+    fighters = db['fighters'] #select collection
+    fighter_list = []
+    for f in fighters.find({},{"_id": 0, "name": 1, "striking": 1, "grappling": 1}):
+        fighter_list.append({'name': f['name'], 'striking': f['striking'], 'grappling': f['grappling']})
+    return jsonify({'fighters' : fighter_list}) 
 
 if __name__ == '__main__':
     app.run(debug=True)
